2007c41cd577dd5ff4218487b546053754e930f9
mariahuertas <mariahuertas296@gmail.com>
2020-02-22 13:59:47 +0100

    Add Color and SecretCombination class - Part 1

--- src/main/scala/models/Color.scala
+++ src/main/scala/models/Color.scala
@@ -1,6 +1,11 @@
 package models
 
-class Color() {
+object Color extends Enumeration {
+  type Color = Value
+  val RED, BLUE, YELLOW, GREEN, ORANGE, PURPLE = Value
+
+  def length: Int = Color.values.size
+
+  // TODO add get random values
 
 }
-
--- src/main/scala/models/SecretCombination.scala
+++ src/main/scala/models/SecretCombination.scala
@@ -1,6 +1,19 @@
 package models
 
-class SecretCombination() {
+import scala.util.Random
+class SecretCombination(secretCombination: List[Color.Color] = Nil) {
+
+  val secretCombination_ =
+    secretCombination match {
+      case Nil => generateSecretCombination
+      case _ => secretCombination
+    }
+
+  def getSecretCombination: List[Color.Color] =
+    secretCombination_
+
+  def generateSecretCombination: List[Color.Color] =
+    (0 to 4).map(_ => (new util.Random).nextInt(4)).toList // TODO move nextgIn to Color enum
 
 }
 
