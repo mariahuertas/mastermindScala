7b06976318b167b4474a259282e5faa36a16b32d
mariahuertas <mariahuertas296@gmail.com>
2020-02-23 18:41:46 +0100

    Add ProposedCombination Class

--- src/main/scala/models/ProposedCombination.scala
+++ src/main/scala/models/ProposedCombination.scala
@@ -1,6 +1,21 @@
 package models
 
-class ProposedCombination() {
+class ProposedCombination(proposedCombination: List[Color.Color],secretCombination: List[Color.Color]) {
+  private val MAX_COLOR = 4
+  val proposedCombination_ = proposedCombination
 
+  def getProposedCombination: List[Color.Color] =
+    proposedCombination_
+
+  def calculateBlackToken(proposedCombination: List[Color.Color], secretCombination: List[Color.Color]): Int = {
+    proposedCombination match {
+      case Nil => 0
+      case head :: tail if head == secretCombination.head => 1 +  calculateBlackToken(tail, secretCombination.tail)
+      case _ :: tail => calculateBlackToken(tail, secretCombination.tail)
+    }
+  }
+
+  def isWinner(): Boolean =
+    calculateBlackToken(proposedCombination, secretCombination) == this.MAX_COLOR
 }
 
